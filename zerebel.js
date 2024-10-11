0bc3fc509798dd18f2a7062789d3e1018795aecd
ushervin <eddyusherjeezy@gmail.com>
2024-10-11 23:50:09 +0200

    first

---
+++ zerebel.js
@@ -0,0 +1,50 @@
+
+
+// let SeasonsByMonth = {
+//    'january': 'winter',
+//    'february': 'winter',
+//    ' march': 'spring',
+//    'april': 'spring',
+//    'may' : 'spring',
+//    'june' : 'summer',
+//    'july' : 'summer',
+//    'august' : 'summer',
+//    'september' : 'autumn',
+//    'october' : 'autumn',
+//    'november' : 'autumn',
+//    'december' : 'winter',
+// }
+// let SeasonName = prompt('enter the month');
+// console.log(The Season is ${SeasonsByMonth[SeasonName.toLowerCase()]}.)
+
+// let DayOfWeek = {
+//     'monday': 'working day',
+//    'tuesday': 'working day',
+//    ' wednesday': 'working day',
+//    'thursday': 'working day',
+//    'friday' : 'working day',
+//    'saturday' : 'weekend',
+//    'sunday' : 'weekend',
+// }
+
+// let DayName = prompt('what day is today?');
+// console.log(${DayName} is ${dayofweek[DayName.toLowerCase()]}.)
+
+
+const NumberofDays = {
+    'january': '31',
+   'february': '29',
+   'march': '31',
+   'april': '30',
+   'may' : '31',
+   'june' : '30',
+   'july' : '31',
+   'august': '31',
+   'september': '30',
+   'october' : '31',
+   'november' : '30',
+   'december' : '31',
+}
+
+const MonthName = prompt("What month do you want its number of days?")
+console.log(${MonthName} has ${numberofdays[MonthName.toLowerCase()]} days.)
